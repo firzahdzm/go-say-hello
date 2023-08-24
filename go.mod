2facd70011820996432258bd5fe74250f173d4ed
Firza Hadzami <virzha2000@gmail.com>
2023-08-24 09:33:23 +0700

    update v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/firzahdzm/go-say-hello
+module github.com/firzahdzm/go-say-hello/v2
 
 go 1.21.0
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string{
-	return "Hello Firza!"
+func SayHello(name string) string{
+	return "Hello " + name
 }
